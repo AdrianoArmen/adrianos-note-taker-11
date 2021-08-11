1415671f2754d003e5bb1804481511b33009fdf1
AdrianoA <adriano.armendaris@gmail.com>
2021-08-11 08:05:03 -0500

    default route added

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ app.get("/api/notes", (req, res) => {
   }
 });
 
+// deafault route added
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
